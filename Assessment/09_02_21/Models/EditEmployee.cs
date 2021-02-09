88d7df57d1c9b32314263e31c2d92b004da3c385
PrasadAmbokar <58807897+PrasadAmbokar@users.noreply.github.com>
2021-02-09 14:58:09 +0530

    Add files via upload

---
+++ Assessment/09_02_21/Models/EditEmployee.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssessmentFinal.Models
+{
+    public class EditEmployee
+    {
+        [Display(Name = "Id")]
+        [Key]
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int DeptId { get; set; }
+
+        public List<SelectListItem> Departments { get; set; }
+
+       // public int DeptNo { get; set; }
+        public string DOB { get; set; }
+        public string DeptName { get; set; }
+        public string Designation { get; set; }
+        public float GrossSalary { get; set; }
+        public int BankAccount { get; set; }
+        public string BankName { get; set; }
+        public string Degree { get; set; }
+        public string POY { get; set; }
+        public string DOJ { get; set; }
+    }
+}
---
+++ Assessment/09_02_21/Models/ShowData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssessmentFinal.Models
+{
+    public class ShowData
+    {
+        [Key]
+        public int EmpId { get; set; }
+        [Required]
+        public string EmpName { get; set; }
+        public string DeptName { get; set; }
+    }
+}
